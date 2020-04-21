20dc186741f95a953b95640fd97df7c9952384bf
Shrimpie <George.nedelev@gmail.com>
2020-04-21 15:44:41 +0200

    Imported Photon2 and creded basic server
    -Menu Scene where the players connect to the server
    -Multiplayer scene where the players spawn and move
    -Several scripts controlling players Avatars / movement availability
    -Basic UI for choosing an avatar (work in progress)

---
+++ Grundfos-VR-salesdata/Assets/Scripts/PhotonScripts/Game controllers/GameSetup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetup : MonoBehaviour
+{
+    public static GameSetup GS = new GameSetup();
+    public Transform[] spawnPoints;
+    // Start is called before the first frame update
+
+    private void OnEnable()
+    {
+        if (GS != null)
+        {
+
+        }
+
+        GS = this;
+
+
+
+    }
+
+
+
+}
