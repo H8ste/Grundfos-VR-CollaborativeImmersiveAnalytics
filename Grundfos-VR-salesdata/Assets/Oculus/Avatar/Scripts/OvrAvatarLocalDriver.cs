ed229cecbfddb64283e65a267a40724620dc3823
Nicklas Olsen <nolse16@student.aau.dk>
2020-05-13 22:59:18 +0200

    Bigtest

--- Grundfos-VR-salesdata/Assets/Oculus/Avatar/Scripts/OvrAvatarLocalDriver.cs
+++ Grundfos-VR-salesdata/Assets/Oculus/Avatar/Scripts/OvrAvatarLocalDriver.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System;
 using System.Collections.Generic;
 using Oculus.Avatar;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class OvrAvatarLocalDriver : OvrAvatarDriver
 {
@@ -95,9 +97,18 @@ public class OvrAvatarLocalDriver : OvrAvatarDriver
         }
     }
 
+
+    public GameObject pvChild = null;
     public override void UpdateTransforms(IntPtr sdkAvatar)
     {
-        CalculateCurrentPose();
-        UpdateTransformsFromPose(sdkAvatar);
+        if (pvChild)
+        {
+            if (pvChild.transform.parent.GetComponent<PhotonView>().IsMine)
+            {
+                CalculateCurrentPose();
+                UpdateTransformsFromPose(sdkAvatar);
+            }
+        }
+
     }
 }
